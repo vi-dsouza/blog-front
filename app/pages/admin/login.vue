33a1c0883825dfca3e7513dbc15344abd91e73a8
Vitoria <vitoriasteffane5@gmail.com>
2026-02-10 17:54:17 -0300

    first commit

---
+++ app/pages/admin/login.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-img
+        cover
+        src="/capa_login.jpg"
+        class="fill-height d-flex align-center justify-center"
+    >
+        <v-card class="containerLogin">
+                <v-btn>Entrar</v-btn>
+        </v-card>
+    </v-img>
+    
+</template>
+
+<style>
+    .fill-height {
+        height: 100vh !important;
+        width: 100vw;
+    }
+    .containerLogin {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        width: 25vw;
+        height: 65vh;
+    }
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  plugins: [
+    '~/plugins/vuetify.ts'
+  ],
+  css: [
+    'vuetify/lib/styles/main.css',
+    '@mdi/font/css/materialdesignicons.min.css'
+  ],
+  build: {
+    transpile: ['vuetify'],
+  },
+  vite: {
+    define: {
+      'process.env.DEBUG': 'false',
+    },
+  },
+})
